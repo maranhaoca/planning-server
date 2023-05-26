59a9d526a6dc007c2a5e4a36a8112507cae3b58d
Caio Siqueira <maranhao.ca@gmail.com>
2023-05-26 10:10:23 +0100

    server running

---
+++ out-tsc/src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, { cors: { origin: 'http://localhost:8000' } });
+const PORT = 3001;
+server.listen(PORT, () => console.log('Server running...'));
+//# sourceMappingURL=index.js.map
